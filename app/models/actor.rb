c4c49907c27e02cdced2b1704153f7b6feda0bcc
Nick Bechtel <bechtel.nick@gmail.com>
2021-12-05 23:32:16 -0500

    commit tree 897e35b65e3c66f48e2fb523bf2fb2f06fe96194

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def list_roles
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
